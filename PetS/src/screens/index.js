54c9fe67067bc6525197fc27741ce4357c9bc0f5
leticia <leticiaae@hotmail.com.br>
2023-04-06 15:25:19 -0300

    added all imports and export

---
+++ PetS/src/screens/index.js
@@ -0,0 +1,13 @@
+import { Begin } from './Begin.js';
+import { CardAnimal } from './CardAnimal.js';
+import { CardAnimalOwner } from './CardAnimalOwner.js';
+import { Home } from './Home.js'
+import { LostPets } from './LostPets.js';
+import { MyLostPets } from './MyLostPets.js';
+import { MyPosts } from './MyPosts.js';
+import { PostPet } from './PostPet.js';
+import { Profile } from './Profile.js';
+import { SignIn } from './SignIn.js';
+import { SignUp } from './SignUp.js';
+
+export { Begin, CardAnimal, CardAnimalOwner, Home, LostPets, MyLostPets, MyPosts, PostPet, Profile, SignIn, SignUp };
