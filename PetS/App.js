260afa57ac011fea6f060b9f72d49cbd8a71ee81
leticia <leticiaae@hotmail.com.br>
2023-04-06 21:19:42 -0300

    added screen

--- PetS/App.js
+++ PetS/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Welcome } from './src/screens/Welcome.js'
+  ;
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <Welcome />
       <StatusBar style="auto" />
-    </View>
+    </>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
