27f38b707edbb59abd2c582e55954679bf9338dc
leticia <leticiaae@hotmail.com.br>
2023-04-06 22:35:54 -0300

    added details

--- PetS/src/screens/Welcome.js
+++ PetS/src/screens/Welcome.js
@@ -9,7 +9,9 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 // imports das imagens do app
 import pet1 from "../../assets/pet1.png";
+import pet2 from "../../assets/pet2.png";
 import logo from "../../assets/Logo.png";
+import pet3 from "../../assets/pet3.png";
 
 export const Welcome = () => {
     // criando a constante para pegar a tela inteira do celular e ocupar na screen da visao
@@ -35,11 +37,14 @@ export const Welcome = () => {
     return (
         <ScrollView
             ref={scrollRef}
+            // ativa o scroll horizontal
             horizontal
+            // faz com que o usuŕaio não passe de tela com o dedo
+            scrollEnabled={true}
             style={{ flex: 1, backgroundColor: "#B67830" }}
         >
             <View style={{ width: sizeScreen }}>
-                <View style={{ alignItems: "center", margin: 70 }}>
+                <View style={{ alignItems: "center", margin: 50 }}>
                     <Image
                         source={logo}
                         style={{ width: 170, height: 167 }}
@@ -53,28 +58,27 @@ export const Welcome = () => {
                         resizeMode="contain"
                     />
                 </View>
-                <View style={{ alignItems: "center", justifyContent: "center" }}>
+                <View style={{ alignItems: "center", justifyContent: "center", marginTop: 60 }}>
                     <Text style={{ color: "#EAEAEA", fontSize: 34 }}>
                         Está à procura de
-                        {"\n"} seu animalzinho de{"\n"} estimação?
+                        {"\n"}seu animalzinho de{"\n"} estimação?
                     </Text>
                 </View>
 
-                <View style={{ height: 84, width: sizeScreen, paddingRight: 20, paddingTop: 30, alignItems: 'flex-end', borderWidth: 2 }}>
+                <View style={{ height: 84, width: sizeScreen, paddingRight: 20, marginTop: 80, alignItems: 'flex-end', borderWidth: 2 }}>
                     <FontAwesome.Button
                         name="chevron-right"
                         color="#B67830"
                         borderRadius={20}
-                        size={49}
+                        size={40}
 
                         style={{
                             alignSelf: "center",
                             backgroundColor: "#FFEDCB",
-
                             height: 100,
-                            width: 150,
+                            width: 100,
                         }}
-                        iconStyle={{ marginHorizontal: 45 }}
+                        iconStyle={{ marginHorizontal: 30 }}
                         onPress={() => setStep(1)}
                     ></FontAwesome.Button>
                 </View>
@@ -83,41 +87,40 @@ export const Welcome = () => {
 
 
             <View style={{ width: sizeScreen }}>
-                <View style={{ alignItems: "center", margin: 70 }}>
+                <View style={{ alignItems: "center", margin: 50 }}>
                     <Image
                         source={logo}
                         style={{ width: 170, height: 167 }}
                         resizeMode="contain"
                     />
                 </View>
-                <View style={{ alignItems: "flex-end", marginRight: 20 }}>
+                <View style={{ alignItems: "flex-start", marginLeft: 80, marginBottom: 20 }}>
                     <Image
-                        style={{ width: 137, height: 104 }}
-                        source={pet1}
+                        style={{ width: 156, height: 156 }}
+                        source={pet2}
                         resizeMode="contain"
                     />
                 </View>
                 <View style={{ alignItems: "center", justifyContent: "center" }}>
                     <Text style={{ color: "#EAEAEA", fontSize: 34 }}>
-                        Aqui você pode{'\n'} encontrar um amigo{"\n"} pra toda a vida!!
+                        Aqui você pode{'\n'}encontrar um amigo{"\n"}pra toda a vida!!
                     </Text>
                 </View>
 
-                <View style={{ height: 84, width: sizeScreen, paddingRight: 20, paddingTop: 30, alignItems: 'flex-end', borderWidth: 2 }}>
+                <View style={{ height: 84, width: sizeScreen, paddingRight: 20, marginTop: 40, alignItems: 'flex-end', borderWidth: 2 }}>
                     <FontAwesome.Button
                         name="chevron-right"
                         color="#B67830"
                         borderRadius={20}
-                        size={49}
+                        size={40}
 
                         style={{
                             alignSelf: "center",
                             backgroundColor: "#FFEDCB",
-
                             height: 100,
-                            width: 150,
+                            width: 100,
                         }}
-                        iconStyle={{ marginHorizontal: 45 }}
+                        iconStyle={{ marginHorizontal: 30 }}
                         onPress={() => setStep(2)}
                     ></FontAwesome.Button>
                 </View>
@@ -126,7 +129,7 @@ export const Welcome = () => {
 
 
             <View style={{ width: sizeScreen }}>
-                <View style={{ alignItems: "center", margin: 70 }}>
+                <View style={{ alignItems: "center", margin: 50 }}>
                     <Image
                         source={logo}
                         style={{ width: 170, height: 167 }}
@@ -135,33 +138,34 @@ export const Welcome = () => {
                 </View>
                 <View style={{ alignItems: "flex-end", marginRight: 20 }}>
                     <Image
-                        style={{ width: 137, height: 104 }}
-                        source={pet1}
+                        style={{ width: 170, height: 148 }}
+                        source={pet3}
                         resizeMode="contain"
                     />
                 </View>
-                <View style={{ alignItems: "center", justifyContent: "center" }}>
-                    <Text style={{ color: "#EAEAEA", fontSize: 34 }}>
-                        Está à procura de
-                        {"\n"} seu animalzinho de{"\n"} estimação?
+                <View style={{ justifyContent: "center" }}>
+                    <Text style={{ paddingLeft: 70, color: "#00D5B0", fontSize: 34, marginBottom: 10 }}>
+                        ADOTE
+                    </Text>
+                    <Text style={{ alignSelf: 'center', color: "#EAEAEA", fontSize: 34 }}>
+                        Amor de verdade é encontrado e não comprado!
                     </Text>
                 </View>
 
-                <View style={{ height: 84, width: sizeScreen, paddingRight: 20, paddingTop: 30, alignItems: 'flex-end', borderWidth: 2 }}>
+                <View style={{ height: 84, width: sizeScreen, paddingRight: 20, marginTop: 30, alignItems: 'flex-end', borderWidth: 2 }}>
                     <FontAwesome.Button
                         name="chevron-right"
                         color="#B67830"
                         borderRadius={20}
-                        size={49}
+                        size={40}
 
                         style={{
                             alignSelf: "center",
                             backgroundColor: "#FFEDCB",
-
                             height: 100,
-                            width: 150,
+                            width: 100,
                         }}
-                        iconStyle={{ marginHorizontal: 45 }}
+                        iconStyle={{ marginHorizontal: 30 }}
                         onPress={() => setStep(3)}
                     ></FontAwesome.Button>
                 </View>
