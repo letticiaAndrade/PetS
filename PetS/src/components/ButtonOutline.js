dfc99e5e79d74708a62420f219a6cc3a3834f3c8
Leticia <leticiaae@hotmail.com.br>
2023-04-07 14:32:14 -0300

    added button outline component

---
+++ PetS/src/components/ButtonOutline.js
@@ -0,0 +1,26 @@
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+
+export default ButtonOutline = ({ text }) => {
+    return (
+        <View style={{ alignItems: "center" }}>
+            <TouchableOpacity style={style.button}>
+                <Text style={style.text}>{text}</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({
+    button: {
+        borderWidth: 1,
+        width: 348,
+        height: 48,
+        borderColor: "#FFEDCB",
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        color: "#FFEDCB",
+    },
+});
