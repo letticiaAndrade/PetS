d9544bfb635ae4a25961c91838fe383dcc7e091d
Leticia <leticiaae@hotmail.com.br>
2023-04-08 19:15:20 -0300

    removed unnecessary parts

--- PetS/src/screens/index.js
+++ PetS/src/screens/index.js
@@ -1,4 +1,4 @@
-import { Begin } from './Begin.js';
+
 import { CardAnimal } from './CardAnimal.js';
 import { CardAnimalOwner } from './CardAnimalOwner.js';
 import { Home } from './Home.js'
@@ -11,4 +11,4 @@ import { SignIn } from './SignIn.js';
 import { SignUp } from './SignUp.js';
 import { Welcome } from './Welcome.js';
 
-export { Begin, CardAnimal, CardAnimalOwner, Home, LostPets, MyLostPets, MyPosts, PostPet, Profile, Welcome, SignIn, SignUp };
+export { CardAnimal, CardAnimalOwner, Home, LostPets, MyLostPets, MyPosts, PostPet, Profile, Welcome, SignIn, SignUp };
