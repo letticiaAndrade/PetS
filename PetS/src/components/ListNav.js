b25360f664a9d925a37f98ace0adec73fc323b2d
Leticia <leticiaae@hotmail.com.br>
2023-04-09 20:45:45 -0300

    added list of category

---
+++ PetS/src/components/ListNav.js
@@ -0,0 +1,55 @@
+import { Card } from "react-native-paper";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+
+export const ListNav = () => {
+    const listCategory = [
+        {
+            _id: "1",
+            image: "",
+            title: "Cães",
+        },
+        {
+            _id: "2",
+            image: "",
+            title: "Gatos",
+        },
+        {
+            _id: "3",
+            image: "",
+            title: "Coelhos",
+        },
+        {
+            _id: "4",
+            image: "",
+            title: "Aves",
+        },
+        {
+            _id: "5",
+            image: "",
+            title: "Répteis",
+        },
+        {
+            _id: "6",
+            image: "",
+            title: "Peixes",
+        },
+    ];
+    return (
+        <View>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                <View>
+                    <TouchableOpacity>
+                        <Card.Content>
+                            <FontAwesome5.Button onPress={""} name="dog">
+
+                            </FontAwesome5.Button>
+                        </Card.Content>
+                        <Card.Title title="Cães" />
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
