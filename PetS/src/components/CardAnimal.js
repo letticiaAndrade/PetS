70c049b5bf745350ce0a4166fcaeb44914681d72
Letícia Andrade <96126803+letticiaAndrade@users.noreply.github.com>
2023-04-14 15:01:09 +0000

    added changes styles

--- PetS/src/components/CardAnimal.js
+++ PetS/src/components/CardAnimal.js
@@ -4,33 +4,33 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 export default CardAnimal = ({ item }) => {
     return (
-        <TouchableOpacity activeOpacity={0.8}>
-            <Card.Content>
-                <View style={{}}>
-                    <Card.Cover
-                        resizeMode="contain"
-                        style={{ width: 142, height: 142 }}
-                        source={{ uri: item?.image }}
-                    />
-                    <View style={{ justifyContent: "center" }}>
-                        <View
-                            style={{
-                                width: 142,
-                                height: 70,
-                                backgroundColor: "#342E29",
-                                opacity: 0.7,
-                                position: "absolute",
-                                alignItems: "baseline",
-                                padding: 5,
-                                borderBottomLeftRadius: 20,
-                                borderBottomRightRadius: 20,
-                            }}
-                        >
-                            <Text style={{ color: "#EAEAEA" }}>{item?.name}</Text>
-                        </View>
+        <TouchableOpacity style={{ overflow: 'hidden', borderRadius: 14, marginHorizontal: 5 }} activeOpacity={0.8}>
+
+            <View style={{ overflow: "hidden" }}>
+                <Card.Cover
+                    resizeMode="contain"
+                    style={{ width: 142, height: 142 }}
+                    source={{ uri: item?.image }}
+                />
+                <View style={{ justifyContent: "center" }}>
+                    <View
+                        style={{
+                            width: 142,
+                            height: 70,
+                            backgroundColor: "#342E29",
+                            opacity: 0.7,
+                            position: "absolute",
+                            alignItems: "baseline",
+                            padding: 5,
+                            borderBottomLeftRadius: 20,
+                            borderBottomRightRadius: 20,
+                        }}
+                    >
+                        <Text style={{ color: "#EAEAEA" }}>{item?.name}</Text>
                     </View>
                 </View>
-            </Card.Content>
+            </View>
+
         </TouchableOpacity>
     );
 };
