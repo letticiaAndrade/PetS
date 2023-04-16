fbfee66b07a34eba3b21027447e1c7c01f290aea
Leticia <leticiaae@hotmail.com.br>
2023-04-16 14:15:58 -0300

    added import and export of new screen

--- PetS/src/screens/index.js
+++ PetS/src/screens/index.js
@@ -6,9 +6,10 @@ import { LostPets } from './LostPets.js';
 import { MyLostPets } from './MyLostPets.js';
 import { MyPosts } from './MyPosts.js';
 import { PostPet } from './PostPet.js';
+import { EditProfile } from './EditProfile.js';
 import { Profile } from './Profile.js';
 import { SignIn } from './SignIn.js';
 import { SignUp } from './SignUp.js';
 import { Welcome } from './Welcome.js';
 
-export { CardAnimal, CardAnimalOwner, Home, LostPets, MyLostPets, MyPosts, PostPet, Profile, Welcome, SignIn, SignUp };
+export { CardAnimal, CardAnimalOwner, Home, LostPets, MyLostPets, MyPosts, PostPet, Profile, Welcome, SignIn, SignUp, EditProfile };
