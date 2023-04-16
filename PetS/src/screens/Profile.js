af50df1a886bcc51772b99ff10be66a040ea925a
Leticia <leticiaae@hotmail.com.br>
2023-04-16 13:01:49 -0300

    added changes styles

--- PetS/src/screens/Profile.js
+++ PetS/src/screens/Profile.js
@@ -2,10 +2,10 @@ import { View, Text } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { TouchableOpacity } from "react-native";
-export const Profile = () => {
+export const Profile = ({ navigation }) => {
     return (
         <View style={{ flex: 1, backgroundColor: '#FFEDCB' }}>
-            <View style={{ flexDirection: 'row', borderWidth: 1, alignItems: 'center' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 20, marginHorizontal: 5 }}>
                 <Ionicons.Button
                     name="arrow-back-circle"
                     backgroundColor={"#FFEDCB"}
@@ -23,23 +23,23 @@ export const Profile = () => {
 
             </View>
             <View>
-                <Text style={{ paddingHorizontal: 15, opacity: 0.7, color: "#342E29", fontSize: 16 }}>Conta</Text>
+                <Text style={{ paddingHorizontal: 20, opacity: 0.7, color: "#342E29", fontSize: 16 }}>Conta</Text>
             </View>
 
-            <TouchableOpacity style={{ marginVertical: 20, flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
-                <Text style={{ fontSize: 16 }}>Meus pets publicados</Text>
-                <FontAwesome name='angle-right' size={24} />
+            <TouchableOpacity style={{ marginVertical: 20, justifyContent: 'space-between', flexDirection: 'row' }} activeOpacity={0.5} onPress={''}>
+                <Text style={{ fontSize: 16, marginLeft: 40 }}>Meus pets publicados</Text>
+                <FontAwesome style={{ marginRight: 40 }} name='angle-right' size={24} />
             </TouchableOpacity>
 
-            <TouchableOpacity style={{ marginVertical: 20, flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
-                <Text style={{ fontSize: 16 }}>Meus pets perdidos</Text>
-                <FontAwesome name='angle-right' size={24} />
+            <TouchableOpacity style={{ marginVertical: 20, justifyContent: 'space-between', flexDirection: 'row' }} activeOpacity={0.5} onPress={''}>
+                <Text style={{ fontSize: 16, marginLeft: 40 }}>Meus pets perdidos</Text>
+                <FontAwesome style={{ marginRight: 40 }} name='angle-right' size={24} />
             </TouchableOpacity>
 
             <View style={{ borderWidth: 0.4, opacity: 0.5, borderColor: '#E4B283' }}></View>
-            <TouchableOpacity style={{ marginVertical: 20, flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
-                <Text style={{ color: '#D45152', fontSize: 16 }}>Sair da conta</Text>
-                <Ionicons name='exit-outline' size={24} color='#D45152' />
+            <TouchableOpacity style={{ marginVertical: 20, justifyContent: 'space-between', flexDirection: 'row' }} activeOpacity={0.5} onPress={''}>
+                <Text style={{ marginLeft: 40, color: '#D45152', fontSize: 16 }}>Sair da conta</Text>
+                <Ionicons style={{ marginRight: 30 }} name='exit-outline' size={24} color='#D45152' />
             </TouchableOpacity>
 
 
