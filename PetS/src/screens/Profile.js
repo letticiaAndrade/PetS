69cd3f1ffd9ba78c6029bc2f3e91b997acf1fc50
Leticia <leticiaae@hotmail.com.br>
2023-04-16 12:39:00 -0300

    added content of conta menu

--- PetS/src/screens/Profile.js
+++ PetS/src/screens/Profile.js
@@ -1,5 +1,7 @@
 import { View, Text } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { TouchableOpacity } from "react-native";
 export const Profile = () => {
     return (
         <View style={{ flex: 1, backgroundColor: '#FFEDCB' }}>
@@ -17,6 +19,29 @@ export const Profile = () => {
 
                 <Text style={{ fontWeight: "500", fontSize: 16, paddingHorizontal: 110 }}>MEU PERFIL</Text>
             </View>
+            <View>
+
+            </View>
+            <View>
+                <Text style={{ padding: 15, opacity: 0.5, color: "#342E29", fontSize: 16 }}>Conta</Text>
+            </View>
+
+            <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
+                <Text style={{ fontSize: 16 }}>Meus pets publicados</Text>
+                <FontAwesome name='angle-right' size={24} />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
+                <Text style={{ fontSize: 16 }}>Meus pets perdidos</Text>
+                <FontAwesome name='angle-right' size={24} />
+            </TouchableOpacity>
+
+            <View style={{ borderWidth: 0.4, opacity: 0.5, borderColor: '#E4B283' }}></View>
+            <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
+                <Text style={{ color: '#D45152', fontSize: 16 }}>Sair da conta</Text>
+                <Ionicons name='exit-outline' size={24} color='#D45152' />
+            </TouchableOpacity>
+
 
 
         </View>
