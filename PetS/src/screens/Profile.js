41e2d0a558af0f7df750ad6d1a29be612e1c5ab8
Leticia <leticiaae@hotmail.com.br>
2023-04-16 12:47:33 -0300

    added changes styles

--- PetS/src/screens/Profile.js
+++ PetS/src/screens/Profile.js
@@ -23,21 +23,21 @@ export const Profile = () => {
 
             </View>
             <View>
-                <Text style={{ padding: 15, opacity: 0.5, color: "#342E29", fontSize: 16 }}>Conta</Text>
+                <Text style={{ paddingHorizontal: 15, opacity: 0.7, color: "#342E29", fontSize: 16 }}>Conta</Text>
             </View>
 
-            <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
+            <TouchableOpacity style={{ marginVertical: 20, flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
                 <Text style={{ fontSize: 16 }}>Meus pets publicados</Text>
                 <FontAwesome name='angle-right' size={24} />
             </TouchableOpacity>
 
-            <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
+            <TouchableOpacity style={{ marginVertical: 20, flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
                 <Text style={{ fontSize: 16 }}>Meus pets perdidos</Text>
                 <FontAwesome name='angle-right' size={24} />
             </TouchableOpacity>
 
             <View style={{ borderWidth: 0.4, opacity: 0.5, borderColor: '#E4B283' }}></View>
-            <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
+            <TouchableOpacity style={{ marginVertical: 20, flexDirection: 'row', justifyContent: 'space-around' }} activeOpacity={0.5} onPress={''}>
                 <Text style={{ color: '#D45152', fontSize: 16 }}>Sair da conta</Text>
                 <Ionicons name='exit-outline' size={24} color='#D45152' />
             </TouchableOpacity>
