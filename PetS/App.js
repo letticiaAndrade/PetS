e985ce79edda2c2f67d85e572a98aabbc9a4aa0a
Leticia <leticiaae@hotmail.com.br>
2023-04-29 16:14:51 -0300

    added provider of react native paper

--- PetS/App.js
+++ PetS/App.js
@@ -3,16 +3,19 @@ import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from "./src/routes"
+import { Provider } from 'react-native-paper';
 
 export default function App() {
   return (
     <>
+      <Provider>
 
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
 
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
+      </Provider>
     </>
 
   );
