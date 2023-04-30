dcb69fba4dade28f456ad76fbf5e8531b5154869
Leticia <leticiaae@hotmail.com.br>
2023-04-29 21:50:07 -0300

    added go back button for screen

--- PetS/src/screens/CardAnimal.js
+++ PetS/src/screens/CardAnimal.js
@@ -1,12 +1,29 @@
-import { View, Text, SafeAreaView } from "react-native";
-
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export const CardAnimal = () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={style.content}>
             <View>
+                <Ionicons.Button
+                    name="arrow-back-circle"
+                    backgroundColor={"#FFEDCB"}
+                    color={"#B67830"}
+                    size={54}
+                    activeOpacity={1}
+                    style={{ width: 60, height: 54 }}
+                    onPress={() => navigation.goBack()}
+                    iconStyle={{ width: 54, height: 54 }}
+                ></Ionicons.Button>
+
                 <Text>Card animal visitante</Text>
             </View>
         </SafeAreaView>
     )
 }
+const style = StyleSheet.create({
+    content: {
+        flex: 1,
+        backgroundColor: '#FFEDCB'
+    },
+})
