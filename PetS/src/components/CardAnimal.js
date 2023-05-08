8ee3e929c970d09750d04818e86e8eaa125a232b
leticia <leticiaae@hotmail.com.br>
2023-05-08 16:20:55 -0300

    added changes styles in card of pet

--- PetS/src/components/CardAnimal.js
+++ PetS/src/components/CardAnimal.js
@@ -1,36 +1,44 @@
 import { Card } from "react-native-paper";
 
-import { Text, TouchableOpacity, View } from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-export default CardAnimal = ({ item, onPress = null }) => {
-    return (
-        <TouchableOpacity style={{ overflow: 'hidden', borderRadius: 14, marginHorizontal: 5 }} onPress={onPress} activeOpacity={0.8}>
-
-            <View style={{ overflow: "hidden" }}>
-                <Card.Cover
-                    resizeMode="contain"
-                    style={{ width: 142, height: 142 }}
-                    source={{ uri: item?.image }}
-                />
-                <View style={{ justifyContent: "center" }}>
-                    <View
-                        style={{
-                            width: 142,
-                            height: 70,
-                            backgroundColor: "#342E29",
-                            opacity: 0.7,
-                            position: "absolute",
-                            alignItems: "baseline",
-                            padding: 5,
-                            borderBottomLeftRadius: 20,
-                            borderBottomRightRadius: 20,
-                        }}
-                    >
-                        <Text style={{ color: "#EAEAEA" }}>{item?.name}</Text>
-                    </View>
-                </View>
+export default CardAnimal = ({ item, onPress = null, sizes = 142 }) => {
+  return (
+    <TouchableOpacity
+      style={{ overflow: "hidden", borderRadius: 14, marginHorizontal: 5, width: sizes, height:sizes }}
+      onPress={onPress}
+      activeOpacity={0.8}
+    >
+      <ImageBackground style={{width: "100%", height: "100%"}} source={{uri: item.image}}>
+            <View
+              style={{
+                width: "100%",
+                backgroundColor: "#00000050",
+                position: "absolute",
+                padding: 5,
+                bottom:0
+              }}
+            >
+              <Text style={{ color: "#fff" }}>{item?.name}</Text>
             </View>
-
-        </TouchableOpacity>
-    );
+            <Ionicons
+              style={{
+                backgroundColor: "#FFEDCB",
+                borderRadius: 20,
+                padding: 2,
+                paddingHorizontal: 3,
+                position: "absolute",
+                bottom: 18,
+                right: 5,
+              }}
+              name={
+                Boolean(item.gender === "F") ? "female-outline" : "male-outline"
+              }
+              color={Boolean(item.gender === "F") ? "red" : "blue"}
+              size={25}
+            />
+      </ImageBackground>
+    </TouchableOpacity>
+  );
 };
