d6cc4f3b328f3195846caeb0fdd196998b484a13
leticia <leticiaae@hotmail.com.br>
2023-05-08 12:40:22 -0300

    added examples

---
+++ PetS/exampleApi.js
@@ -0,0 +1,14 @@
+export const pets = [
+  {
+    _id: 1,
+    image:
+      "https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    name: "Lucca",
+    gender: "M",
+    description: "Um cachorro com cara de pidão jkjasdkjasdklskjlksajdkljkljslkdjklasdklj",
+    phone: 799999999,
+    address: "Rua santa dos dogs",
+    category: 1,
+    ownerName: "Larissa Matos"
+  },
+];
