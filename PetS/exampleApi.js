7f97e648934c55af32ee7a0048a4e77f9132fe52
leticia <leticiaae@hotmail.com.br>
2023-05-08 17:03:05 -0300

    added more examples

--- PetS/exampleApi.js
+++ PetS/exampleApi.js
@@ -11,4 +11,64 @@ export const pets = [
     category: 1,
     ownerName: "Larissa Matos"
   },
+  {
+    _id: 1,
+    image:
+    "https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    name: "Jennie",
+    gender: "F",
+    description: "Um cachorro com cara de pidão jkjasdkjasdklskjlksajdkljkljslkdjklasdklj",
+    phone: 799999999,
+    address: "Rua santa dos dogs",
+    category: 2,
+    ownerName: "Larissa Matos"
+  },
+  {
+    _id: 1,
+    image:
+    "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1143&q=80",
+    name: "Jujuba",
+    gender: "F",
+    description: "Um cachorro com cara de pidão jkjasdkjasdklskjlksajdkljkljslkdjklasdklj",
+    phone: 799999999,
+    address: "Rua santa dos dogs",
+    category: 3,
+    ownerName: "Larissa Matos"
+  },
+  {
+    _id: 1,
+    image:
+      "https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    name: "Jhon",
+    gender: "M",
+    description: "Um cachorro com cara de pidão jkjasdkjasdklskjlksajdkljkljslkdjklasdklj",
+    phone: 799999999,
+    address: "Rua santa dos dogs",
+    category: 4,
+    ownerName: "Larissa Matos"
+  },
+  {
+    _id: 1,
+    image:
+    "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    name: "Mel",
+    gender: "F",
+    description: "Um cachorro com cara de pidão jkjasdkjasdklskjlksajdkljkljslkdjklasdklj",
+    phone: 799999999,
+    address: "Rua santa dos dogs",
+    category: 5,
+    ownerName: "Larissa Matos"
+  },
+  {
+    _id: 1,
+    image:
+    "https://images.unsplash.com/photo-1615497001839-b0a0eac3274c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    name: "Klaus",
+    gender: "M",
+    description: "Um cachorro com cara de pidão jkjasdkjasdklskjlksajdkljkljslkdjklasdklj",
+    phone: 799999999,
+    address: "Rua santa dos dogs",
+    category: 1,
+    ownerName: "Larissa Matos"
+  },
 ];
