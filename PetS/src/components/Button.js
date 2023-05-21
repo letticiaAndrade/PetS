23fcbc4517f7bebdd1b7bdc416deb0160bcdaa77
leticia <leticiaae@hotmail.com.br>
2023-05-21 16:16:22 -0300

    added styles for loading button

--- PetS/src/components/Button.js
+++ PetS/src/components/Button.js
@@ -1,27 +1,43 @@
-import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
-
-export default Button = ({ text, onPress }) => {
-    return (
-        <View style={{ alignItems: "center" }}>
-            <TouchableOpacity onPress={onPress} activeOpacity={0.7} style={style.button}>
-                <Text style={style.text}>{text}</Text>
-            </TouchableOpacity>
-        </View>
-    );
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
+/* import { ActivityIndicator } from "react-native-paper"; */
+export default Button = ({ text, onPress, disabled, animating, isLoading }) => {
+  return (
+    <View style={{ alignItems: "center" }}>
+      <TouchableOpacity
+        onPress={onPress}
+        disabled={disabled}
+        activeOpacity={0.7}
+        style={style.button}
+      >
+        {isLoading ? (
+          <ActivityIndicator animating={animating} />
+        ) : (
+          <Text style={style.text}>{text}</Text>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const style = StyleSheet.create({
-    button: {
-        backgroundColor: "#B67830",
-        width: 348,
-        height: 48,
-        elevation: 10,
-        borderRadius: 10,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  button: {
+    backgroundColor: "#B67830",
+    width: 348,
+    height: 48,
+    elevation: 10,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
 
-    text: {
-        color: "#FFEDCB",
-    },
+  text: {
+    color: "#FFEDCB",
+  },
 });
