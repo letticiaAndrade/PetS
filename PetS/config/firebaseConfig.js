6878906b9dd510beed0d843332df74029fc04007
leticia <leticiaae@hotmail.com.br>
2023-05-27 16:42:38 -0300

    added cloud storage

--- PetS/config/firebaseConfig.js
+++ PetS/config/firebaseConfig.js
@@ -12,7 +12,7 @@ import { initializeApp } from "firebase/app";
 // Optionally import the services that you want to use
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import {getStorage} from "firebase/storage";
 // Initialize Firebase
 const firebaseConfig = {
   apiKey: API_KEY,
@@ -21,11 +21,13 @@ const firebaseConfig = {
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
   appId: APP_ID,
+  storageBucket: STORAGE_BUCKET
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
 export const database = getFirestore();
+export const storage = getStorage(firebaseApp);
 
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
