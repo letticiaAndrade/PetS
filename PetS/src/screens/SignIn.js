b7a966b146e3c2a28235977c6797b38360e68bc2
leticia <leticiaae@hotmail.com.br>
2023-06-01 15:55:17 -0300

    added user login cache saving

--- PetS/src/screens/SignIn.js
+++ PetS/src/screens/SignIn.js
@@ -9,6 +9,8 @@ import {
   ScrollView,
 } from "react-native";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { useState } from "react";
 
 // imports de componentes personalizados
@@ -55,8 +57,7 @@ export const SignIn = ({ navigation }) => {
         const authUser = userCredential?.user;
         const userRef = doc(database, "users", authUser?.uid);
         const userData = await getDoc(userRef);
-        // guardar userData no cache
-
+        await AsyncStorage.setItem("@session", JSON.stringify(userData.data()) );
         navigation.navigate("Routes");
       })
       .catch(() => console.warn("Ocorreu um erro"))
@@ -68,133 +69,133 @@ export const SignIn = ({ navigation }) => {
   return (
     <SafeAreaView style={style.content}>
       <ScrollView>
-      <View
-        style={{
-          paddingHorizontal: 15,
-          paddingVertical: 10,
-          alignItems: "center",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <View>
-          <Ionicons.Button
-            name="arrow-back-circle"
-            backgroundColor={"#FFEDCB"}
-            color={"#B67830"}
-            size={54}
-            activeOpacity={1}
-            style={{ width: 60, height: 54 }}
-            onPress={() => navigation.goBack()}
-            iconStyle={{ width: 54, height: 54 }}
-          ></Ionicons.Button>
-        </View>
-        <View>
-          <Image
-            source={Logo}
-            resizeMode="contain"
-            style={{ width: 94, height: 92 }}
-          />
-        </View>
-      </View>
-
-      <View style={{ margin: 30 }}>
-        <Text
+        <View
           style={{
-            color: "#342E29",
-            fontWeight: "bold",
-            fontSize: 32,
-            marginBottom: 10,
+            paddingHorizontal: 15,
+            paddingVertical: 10,
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
           }}
         >
-          Bem vindo de volta!
-        </Text>
-        <Text style={{ color: "#342E29", opacity: 0.6, fontSize: 20 }}>
-          Preencha os campos{"\n"}para entrar na conta.
-        </Text>
-      </View>
-
-      <View style={{ marginHorizontal: 30, marginVertical: 10 }}>
-        <Controller
-          name="email"
-          control={control}
-          rules={{
-            required: "Campo obrigatório.",
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              value={value}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              error={Boolean(errors?.email)}
-              label="Email"
-              mode="outlined"
-              activeOutlineColor="#342E29"
-              left={<TextInput.Icon icon="email-outline" />}
+          <View>
+            <Ionicons.Button
+              name="arrow-back-circle"
+              backgroundColor={"#FFEDCB"}
+              color={"#B67830"}
+              size={54}
+              activeOpacity={1}
+              style={{ width: 60, height: 54 }}
+              onPress={() => navigation.goBack()}
+              iconStyle={{ width: 54, height: 54 }}
+            ></Ionicons.Button>
+          </View>
+          <View>
+            <Image
+              source={Logo}
+              resizeMode="contain"
+              style={{ width: 94, height: 92 }}
             />
-          )}
-        />
-        {Boolean(errors?.email) && (
-          <HelperText type="error" visible={Boolean(errors?.email)}>
-            {errors?.email?.message}
-          </HelperText>
-        )}
-      </View>
+          </View>
+        </View>
 
-      <View style={{ marginHorizontal: 30, marginVertical: 10 }}>
-        <Controller
-          name="password"
-          control={control}
-          rules={{ required: "Campo obrigatório." }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              value={value}
-              onBlur={onBlur}
-              onChangeText={onChange}
-              error={Boolean(errors?.password)}
-              selectionColor="#FFEDCB"
-              activeOutlineColor="#342E29"
-              secureTextEntry={true}
-              label="Senha"
-              mode="outlined"
-              textColor="#342E29"
-              activeUnderlineColor="#FFEDCB"
-              left={<TextInput.Icon icon="lock-outline" />}
-            />
-          )}
-        />
-        {Boolean(errors?.password) && (
-          <HelperText type="error" visible={Boolean(errors?.password)}>
-            {errors?.password?.message}
-          </HelperText>
-        )}
-      </View>
-      <View style={{ marginVertical: 10 }}>
-        <Button
-          animating={isLoading ? true : false}
-          isLoading={isLoading}
-          disabled={isLoading}
-          onPress={handleSubmit(onSubmit)}
-          text={"ENTRAR"}
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          marginVertical: 30,
-        }}
-      >
-        <Text style={{ color: "#342E29", fontSize: 20, paddingRight: 10 }}>
-          Não tem uma conta?
-        </Text>
-        <TouchableOpacity onPress={() => navigation.navigate(SignUp)}>
-          <Text style={{ color: "#B67830", fontSize: 20, letterSpacing: 1 }}>
-            Registre-se
+        <View style={{ margin: 30 }}>
+          <Text
+            style={{
+              color: "#342E29",
+              fontWeight: "bold",
+              fontSize: 32,
+              marginBottom: 10,
+            }}
+          >
+            Bem vindo de volta!
           </Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+          <Text style={{ color: "#342E29", opacity: 0.6, fontSize: 20 }}>
+            Preencha os campos{"\n"}para entrar na conta.
+          </Text>
+        </View>
+
+        <View style={{ marginHorizontal: 30, marginVertical: 10 }}>
+          <Controller
+            name="email"
+            control={control}
+            rules={{
+              required: "Campo obrigatório.",
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                value={value}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                error={Boolean(errors?.email)}
+                label="Email"
+                mode="outlined"
+                activeOutlineColor="#342E29"
+                left={<TextInput.Icon icon="email-outline" />}
+              />
+            )}
+          />
+          {Boolean(errors?.email) && (
+            <HelperText type="error" visible={Boolean(errors?.email)}>
+              {errors?.email?.message}
+            </HelperText>
+          )}
+        </View>
+
+        <View style={{ marginHorizontal: 30, marginVertical: 10 }}>
+          <Controller
+            name="password"
+            control={control}
+            rules={{ required: "Campo obrigatório." }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                value={value}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                error={Boolean(errors?.password)}
+                selectionColor="#FFEDCB"
+                activeOutlineColor="#342E29"
+                secureTextEntry={true}
+                label="Senha"
+                mode="outlined"
+                textColor="#342E29"
+                activeUnderlineColor="#FFEDCB"
+                left={<TextInput.Icon icon="lock-outline" />}
+              />
+            )}
+          />
+          {Boolean(errors?.password) && (
+            <HelperText type="error" visible={Boolean(errors?.password)}>
+              {errors?.password?.message}
+            </HelperText>
+          )}
+        </View>
+        <View style={{ marginVertical: 10 }}>
+          <Button
+            animating={isLoading ? true : false}
+            isLoading={isLoading}
+            disabled={isLoading}
+            onPress={handleSubmit(onSubmit)}
+            text={"ENTRAR"}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginVertical: 30,
+          }}
+        >
+          <Text style={{ color: "#342E29", fontSize: 20, paddingRight: 10 }}>
+            Não tem uma conta?
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate(SignUp)}>
+            <Text style={{ color: "#B67830", fontSize: 20, letterSpacing: 1 }}>
+              Registre-se
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
