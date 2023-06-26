497d9568a8dd7f4392219a2de3ec0dd91b4fb7af
leticia <leticiaae@hotmail.com.br>
2023-06-26 12:37:32 -0300

    added decode

--- PetS/App.js
+++ PetS/App.js
@@ -5,6 +5,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
 import { Provider } from "react-native-paper";
 
+import { decode } from "base-64";
+
+if (typeof atob === "undefined") {
+  global.atob = decode;
+}
+
 export default function App() {
   return (
     <>
