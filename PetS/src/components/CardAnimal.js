772fadc7b705a67b916f65833f9435eaa31d5cfa
leticia <leticiaae@hotmail.com.br>
2023-06-26 16:59:16 -0300

    added isLost boolean

--- PetS/src/components/CardAnimal.js
+++ PetS/src/components/CardAnimal.js
@@ -4,43 +4,62 @@ import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
 
-
-export default CardAnimal = ({ item, onPress = null, sizes = 142, style }) => {
+export default CardAnimal = ({
+  item,
+  isLost = false,
+  onPress = null,
+  sizes = 142,
+  style,
+}) => {
   const navigation = useNavigation();
   return (
     <TouchableOpacity
-      style={{ overflow: "hidden", borderRadius: 14, marginHorizontal: 5, width: sizes, height:sizes, ...style }}
-      onPress={()=> navigation.navigate("CardAnimal", {pet: item})}
+      style={{
+        overflow: "hidden",
+        borderRadius: 14,
+        marginHorizontal: 5,
+        width: sizes,
+        height: sizes,
+        ...style,
+      }}
+      onPress={() =>
+        navigation.navigate("CardAnimal", { pet: item, isLost: isLost })
+      }
       activeOpacity={0.8}
     >
-      <ImageBackground style={{width: "100%", height: "100%"}} source={{uri: item.image}}>
-            <View
-              style={{
-                width: "100%",
-                backgroundColor: "#00000050",
-                position: "absolute",
-                padding: 5,
-                bottom:0
-              }}
-            >
-              <Text style={{ color: "#fff" }}>{item?.name}</Text>
-            </View>
-            <Ionicons
-              style={{
-                backgroundColor: "#FFEDCB",
-                borderRadius: 20,
-                padding: 2,
-                paddingHorizontal: 3,
-                position: "absolute",
-                bottom: 18,
-                right: 5,
-              }}
-              name={
-                Boolean(item.gender === "Feminino") ? "female-outline" : "male-outline"
-              }
-              color={Boolean(item.gender === "Feminino") ? "red" : "blue"}
-              size={25}
-            />
+      <ImageBackground
+        style={{ width: "100%", height: "100%" }}
+        source={{ uri: item.image }}
+      >
+        <View
+          style={{
+            width: "100%",
+            backgroundColor: "#00000050",
+            position: "absolute",
+            padding: 5,
+            bottom: 0,
+          }}
+        >
+          <Text style={{ color: "#fff" }}>{item?.name}</Text>
+        </View>
+        <Ionicons
+          style={{
+            backgroundColor: "#FFEDCB",
+            borderRadius: 20,
+            padding: 2,
+            paddingHorizontal: 3,
+            position: "absolute",
+            bottom: 18,
+            right: 5,
+          }}
+          name={
+            Boolean(item.gender === "Feminino")
+              ? "female-outline"
+              : "male-outline"
+          }
+          color={Boolean(item.gender === "Feminino") ? "red" : "blue"}
+          size={25}
+        />
       </ImageBackground>
     </TouchableOpacity>
   );
